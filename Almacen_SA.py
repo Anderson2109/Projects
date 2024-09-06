308b61acc868fd354953a6f630b18866a9d68265
Anderson0921 <andersonjiel842@gmail.com>
2024-09-05 20:38:12 -0400

    funcion de agregar item

--- Almacen_SA.py
+++ Almacen_SA.py
@@ -8,3 +8,11 @@ class Almacen:
         self.capacidad_total = self.altura * self.anchura * self.largura
         self.capacidad_disponible = self.capacidad_total * 0.80 
         self.items = []
+
+    def agregar_item(self, item):
+        if item.area <= self.capacidad_disponible:
+            self.items.append(item)
+            self.capacidad_disponible -= item.area
+            print(f"Item {item.descripcion} agregado al almacén {self.nombre}.")
+        else:
+            print(f"Capacidad insuficiente para el item {item.descripcion}.")
